254196ddc86fa1216baf43a10705559dca8143d1
Ryan Fitzer <rfitzer@sapient.com>
2016-02-04 21:17:39 -0800

    Rename to match the file that's using it (since they're temporary).

---
+++ runme-examples.js
@@ -0,0 +1,100 @@
+// Just some dumb code
+for (var i = 0; i < 10; i++) {
+    // do something
+}
+
+
+
+    /**
+     * This is the general description.
+     *
+     *     This is a tabbed line (4 spaces)
+     *     This is a tabbed line (4 spaces)
+     *
+     * @title One Element
+     * @example The description of the example
+     *  <div class="flash-block">
+     *      <div class="flash-block-content">
+     *          Success Message
+     *      </div>
+     *  </div>
+     * @modifier .flash-block-success JS added class that disables the block disappearing
+     * @state :focus Special animated hover focus state
+     * @state :hover Special animated hover focus state
+     */
+    var ryan = 'one';
+
+/**
+ * This is the general description.
+ *
+ * Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ *
+ *     This is a tabbed line (4 spaces)
+ *     This is a tabbed line (4 spaces)
+ *
+ * @title Button
+ *
+ * @modifier .cta primary coloring
+ * @state :hover transition animation on hover
+ *
+ * @example The example description
+ *  <a class="btn" href="#">a.btn</a>
+ *
+ * @example <button class="btn">button.btn</button>
+ *
+ * @example <button class="btn cta">button.btn.cta</button>
+ *
+ * @example
+ *  <div class="btn-set">
+ *    <button class="btn cta">button.btn.cta</button>
+ *    <button class="btn">button.btn</button>
+ *  </div>
+ */
+var ryan = 'two';
+
+/**
+ * This is the general description.
+ *
+ * @title Three Element
+ * @example
+ *  <div class="flash-block">
+ *      <div class="flash-block-content">
+ *          Success Message
+ *      </div>
+ *  </div>
+ * @modifier .flash-block-success JS added class that disables the block disappearing
+ * @state :focus Special animated hover focus state
+ * @state :hover Special animated hover focus state
+ */
+var ryan = 'three';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * This is the general description.
+ *
+ * @title Four Element
+ * @example
+ *  <div class="flash-block">
+ *      <div class="flash-block-content">
+ *          Success Message
+ *      </div>
+ *  </div>
+ * @modifier .flash-block-success JS added class that disables the block disappearing
+ * @state :focus Special animated hover focus state
+ * @state :hover Special animated hover focus state
+ */
+var ryan = 'four';
+
+
